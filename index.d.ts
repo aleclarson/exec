c3832361b084d1c89b2e40033a7e059bf7088817
Alec Larson <alec.stanford.larson@gmail.com>
2019-10-21 15:59:34 -0400

    feat: vastly improved types

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,26 @@
-declare const exec: {
-  /** Execute a shell command asynchronously */
-  (cmd: string, ...args: any[]): Promise<string>
+import { SpawnOptions } from "child_process"
+
+/** Execute a shell command asynchronously */
+declare const exec: Exec & {
   /** Execute a shell command synchronously */
-  sync(cmd: string, ...args: any[]): string
+  sync: SyncExec
   /** Execute a shell command asynchronously */
-  async: typeof exec
+  async: Exec
 }
+
+declare namespace exec {
+  export type Exec = (cmd: string, ...args: Args) => Promise<string>
+  export type Args = Array<Argv | Options | Listener>
+
+  // Synchronous version
+  export type SyncExec = (cmd: string, ...args: SyncArgs) => string
+  export type SyncArgs = Array<Argv | Options>
+
+  // Argument types
+  export type Arg = string | null | undefined
+  export type Argv = Array<Arg | Arg[]>
+  export type Options = SpawnOptions
+  export type Listener = (stderr: string, stdout: string) => void
+}
+
 export = exec
