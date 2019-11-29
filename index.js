c98cfa8b401697551440c7572dbc580e799cb224
Alec Larson <alec.stanford.larson@gmail.com>
2019-11-28 22:00:06 -0500

    chore: use prettier

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const {spawn, spawnSync} = require('child_process');
-const TypeError = require('type-error');
-const fs = require('fs');
+const { spawn, spawnSync } = require('child_process')
+const TypeError = require('type-error')
+const fs = require('fs')
 
 function execAsync(...args) {
-  return exec(false, ...args);
+  return exec(false, ...args)
 }
 
 function execSync(...args) {
-  return exec(true, ...args);
+  return exec(true, ...args)
 }
 
-module.exports = execAsync;
-module.exports.sync = execSync;
-module.exports.async = execAsync;
+module.exports = execAsync
+module.exports.sync = execSync
+module.exports.async = execAsync
 
 //
 // Helpers
@@ -22,29 +22,31 @@ const trailingNewlines = /[\r\n]+$/
 
 function stripTrailingNewlines(str) {
   if (typeof str == 'string') {
-    return str.replace(trailingNewlines, '');
-  } else return str;
+    return str.replace(trailingNewlines, '')
+  } else return str
 }
 
 function isDir(dir) {
   try {
-    return fs.statSync(dir).isDirectory();
-  } catch(e) { return false; }
+    return fs.statSync(dir).isDirectory()
+  } catch (e) {
+    return false
+  }
 }
 
 function exec(sync, cmd, ...args) {
   if (typeof cmd !== 'string') {
-    throw TypeError(String, cmd);
+    throw TypeError(String, cmd)
   }
 
   // TODO: support escaped spaces?
-  cmd = cmd.split(' ');
+  cmd = cmd.split(' ')
 
-  const opts = {};
+  const opts = {}
   args.forEach(arg => {
-    if (arg == null) return;
+    if (arg == null) return
     if (arg.constructor == Object) {
-      Object.assign(opts, arg);
+      Object.assign(opts, arg)
     } else if (Array.isArray(arg)) {
       arg.forEach(arg => {
         if (arg == null) return
@@ -53,96 +55,94 @@ function exec(sync, cmd, ...args) {
         } else {
           cmd.push(String(arg))
         }
-      });
+      })
     } else if (!sync && typeof arg == 'function') {
-      opts.listener = arg;
+      opts.listener = arg
     } else {
-      cmd.push(String(arg));
+      cmd.push(String(arg))
     }
-  });
+  })
 
   if (opts.cwd && !isDir(opts.cwd)) {
-    const error = Error('Directory does not exist: ' + opts.cwd);
-    error.code = 'ENOTDIR';
-    throw error;
+    const error = Error('Directory does not exist: ' + opts.cwd)
+    error.code = 'ENOTDIR'
+    throw error
   }
 
   // Default to utf8 string.
-  opts.encoding || (opts.encoding = 'utf8');
+  opts.encoding || (opts.encoding = 'utf8')
 
   if (sync) {
-    const proc = spawnSync(cmd.shift(), cmd, opts);
+    const proc = spawnSync(cmd.shift(), cmd, opts)
     if (proc.error) {
       if (proc.error.code == 'ENOENT') {
-        proc.error.message = 'Unknown command: ' + cmd;
+        proc.error.message = 'Unknown command: ' + cmd
       }
-      throw proc.error;
+      throw proc.error
     }
     if (proc.status != 0) {
       const msg = proc.stderr
         ? stripTrailingNewlines(proc.stderr)
-        : 'Closed with non-zero exit code: ' + proc.status;
+        : 'Closed with non-zero exit code: ' + proc.status
 
-      const error = new Error(msg);
-      error.exitCode = proc.status;
-      throw error;
+      const error = new Error(msg)
+      error.exitCode = proc.status
+      throw error
     }
-    return stripTrailingNewlines(proc.stdout);
-  }
-  else {
+    return stripTrailingNewlines(proc.stdout)
+  } else {
     // Capture a useful stack trace.
-    const error = new Error();
+    const error = new Error()
     return new Promise((resolve, reject) => {
-      const proc = spawn(cmd.shift(), cmd, opts);
+      const proc = spawn(cmd.shift(), cmd, opts)
 
-      let failed = false;
+      let failed = false
       proc.on('error', e => {
-        Object.assign(error, e);
-        error.message = e.code == 'ENOENT'
-          ? 'Unknown command: ' + cmd
-          : e.message;
+        Object.assign(error, e)
+        error.message =
+          e.code == 'ENOENT' ? 'Unknown command: ' + cmd : e.message
 
-        failed = true;
-        reject(error);
-      });
+        failed = true
+        reject(error)
+      })
 
       if (opts.listener) {
-        proc.stdout.on('data', data => opts.listener(null, data));
-        proc.stdout.setEncoding(opts.encoding);
+        proc.stdout.on('data', data => opts.listener(null, data))
+        proc.stdout.setEncoding(opts.encoding)
 
-        proc.stderr.on('data', opts.listener);
-        proc.stderr.setEncoding(opts.encoding);
+        proc.stderr.on('data', opts.listener)
+        proc.stderr.setEncoding(opts.encoding)
 
         proc.on('close', code => {
-          if (failed) return;
-          if (code == 0) return resolve();
-          error.message = 'Closed with non-zero exit code: ' + code;
-          error.exitCode = code;
-          reject(error);
-        });
-      }
-      else {
-        const stdout = [], stderr = [];
+          if (failed) return
+          if (code == 0) return resolve()
+          error.message = 'Closed with non-zero exit code: ' + code
+          error.exitCode = code
+          reject(error)
+        })
+      } else {
+        const stdout = [],
+          stderr = []
         if (proc.stdout) {
-          proc.stdout.on('data', data => stdout.push(data));
+          proc.stdout.on('data', data => stdout.push(data))
         }
         if (proc.stderr) {
-          proc.stderr.on('data', data => stderr.push(data));
+          proc.stderr.on('data', data => stderr.push(data))
         }
         proc.on('close', code => {
-          if (failed) return;
+          if (failed) return
           if (code == 0) {
-            resolve(stripTrailingNewlines(stdout.join('')));
+            resolve(stripTrailingNewlines(stdout.join('')))
           } else {
             error.message = stderr.length
               ? stripTrailingNewlines(stderr.join(''))
-              : 'Closed with non-zero exit code: ' + code;
+              : 'Closed with non-zero exit code: ' + code
 
-            error.exitCode = code;
-            reject(error);
+            error.exitCode = code
+            reject(error)
           }
-        });
+        })
       }
-    });
+    })
   }
 }
